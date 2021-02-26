4152c719335bca8c5289d13d5325902a76111d89
Lizievisdddd <lizievi1123@gmail.com>
2021-02-25 20:04:43 -0500

    1st commit

---
+++ Omnifood/resources/js/script.js
@@ -0,0 +1,52 @@
+ $(document).ready(function(){
+
+  // $('h1').click(function(){
+  //   $(this).css('background-color', '#ff0000');
+
+  // });
+    /*var waypoints = $('#handler-first').waypoint(function(direction) {
+        notify(this.element.id + ' hit 25% from top of window') 
+        }, {
+       offset: '25%'
+    })*/
+
+    // 
+/* FOR THE STICKY NAVIGATION*/ 
+  $('.js--section-features').waypoint(function(direction){
+    if (direction == "down"){
+      $('nav').addClass('sticky');
+    } else{
+      $('nav').removeClass('sticky');
+    }
+  }, {
+      offset: '60px;'
+  });
+
+/*SCROLL ON BUTTONS */
+$('.js--scroll-to-plans').click(function(){
+  $('html, body').animate({scrollTop: $('.js--section-plans').offset().top},1000);
+})
+
+$('.js--scroll-to-start').click(function(){
+  $('html, body').animate({scrollTop: $('.js--section-features').offset().top},1000);
+})
+
+// NAVIGATION SCROLL
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
+
+ });
+ 
